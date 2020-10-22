6b3e4dbf3f39e8dedb4efd9b54ce709a4022f92a
hzh1019 <653410@qq.com>
2020-10-22 11:13:51 +0800

    Create win10PicsExport.bat
    自动导出Win10锁屏壁纸至系统盘根目录下的pics目录内。

---
+++ win10PicsExport.bat
@@ -0,0 +1,10 @@
+@REM win10锁屏壁纸导出脚本,默认保存在系统盘根目录下的pics内，修改picdir变量可变更保存路径。
+set picdir=%SystemDrive%\pics
+if not exist %picdir% (md %picdir%) else (rd /s /Q %picdir% && md %picdir%)
+cd /d %userprofile%
+for /r /d %%i in (*Windows.Content*) do cd /d %%i\LocalState\Assets
+copy * %picdir%
+cd /d %picdir%
+rename * *.jpg
+cd /
+start %picdir%
