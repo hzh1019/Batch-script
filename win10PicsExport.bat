68814fef78af80125b4f0ff621b866a37c2d11cf
hzh1019 <653410@qq.com>
2020-10-22 11:22:22 +0800

    Update win10PicsExport.bat

--- win10PicsExport.bat
+++ win10PicsExport.bat
@@ -1,4 +1,5 @@
 @REM win10锁屏壁纸导出脚本,默认保存在系统盘根目录下的pics内，修改picdir变量可变更保存路径。
+@REM Automatically export win10 lock screen wallpaper
 set picdir=%SystemDrive%\pics
 if not exist %picdir% (md %picdir%) else (rd /s /Q %picdir% && md %picdir%)
 cd /d %userprofile%
